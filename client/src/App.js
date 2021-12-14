cb0d7241693da7d9cb21decf0e333d6e5fd3c32d
Esoolgnah <nezcoreen@gmail.com>
2021-12-14 15:26:34 +0900

    Setting client directory

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Header from '../src/components/Header.js'
+import Saying from '../src/components/Saying.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path='/'>
+          <Saying />
+        </Route>
+      </Switch>
+      <NotificationCenter />
+    </Router>
   );
 }
 
---
+++ client/src/components/Saying.js
@@ -0,0 +1,5 @@
+import './App.css';
+function Saying(){
+
+}
+export default Saying;
