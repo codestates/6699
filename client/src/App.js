2b463bfe39646f31bf9c0321b0785ac114e6f6bb
sun0-1106 <sy.choi1106@gmail.com>
2021-12-16 20:16:02 +0900

    Make PostingPage

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className='container'>
       <Header />
-      <MainPagePlusButton/>
+  
       <div className='header-downside'>
         <Routes>
           <Route exact path='/' element={<LandingPage/>}/>
