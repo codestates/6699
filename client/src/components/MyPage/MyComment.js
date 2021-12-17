0d6c11bb52e9d3d045283758d2a0f17012c10caa
sun0-1106 <sy.choi1106@gmail.com>
2021-12-18 02:01:00 +0900

    Midight merge

--- client/src/components/MyPage/MyComment.js
+++ client/src/components/MyPage/MyComment.js
@@ -1,9 +1,21 @@
 import style from './MyComment.module.css'
+import MyCommentBox from './MyCommentBox'
+import PageNation from './MyPagePagenation'
 function MyComment(){
     return (
-    <div className={style.box}>
-   
-   </div>
+<div id={style.container}>
+<MyCommentBox/>
+<MyCommentBox/>
+<MyCommentBox/>
+<MyCommentBox/>
+<MyCommentBox/>
+<div className={style.outer_wrapper}>
+<div id={style.pagenation_wrapper}>
+<PageNation/>
+</div>
+</div>
+</div>
+       
     )
 }
 export default MyComment;
