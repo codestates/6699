5fba29c0207c68354b31f574768516ed7619b538
sun0-1106 <sy.choi1106@gmail.com>
2021-12-21 10:51:44 +0900

    Make merge

--- client/src/components/PostingPage/PostingMakeCommentBox.js
+++ client/src/components/PostingPage/PostingMakeCommentBox.js
@@ -6,7 +6,7 @@ function PostingMakeCommentBox(){
   <div className={style.comment_writting_container}>
   <div id={style.writting_comment_border}>
    <div id={style.user_image}></div>
-   <input placeholder='나꼬북(으)로 댓글달기...'></input>
+   <input placeholder='댓글을 작성해주세요'></input>
   </div>
   <button id={style.make_comment}>게시하기</button>
   </div>
